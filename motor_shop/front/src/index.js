27c149d94f5a9959a18d96edcf0c5bdb24278795
Pedro <pedro@gmail.com>
2022-08-11 13:21:56 -0300

    feat: created card profile on page

--- motor_shop/front/src/index.js
+++ motor_shop/front/src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import GlobalStyle from '../src/styles/global.js'
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import GlobalStyle from "./styles/global.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
